ed53a498c0f667eb441fb6a040445ced9b4f7e66
Clement Glt <guillemot.clement@protonmail.com>
2024-03-30 17:18:06 +0100

    🎨 Recup des données et opti conversion json

--- src/form/form.js
+++ src/form/form.js
@@ -8,41 +8,18 @@ form.addEventListener('submit', event => {
     //on empeche rechargement par défaut de la page
     event.preventDefault();
 
-    //on viens créer un nouvel FormData pour récupérer facilement les valeurs du formulaire
+    //on récupère les données du formulaire
     const formData = new FormData(form);
-    //on les convertis ensuite en JSON pour les envoyer sur le serveur
-    //entries() retourne un objet itérable
-    //la variable entries stocke les input
-    const entries = formData.entries();
-    
-    //on parcours le tableau avec une boucle for
-    // for(let entry of entries){
-    //     //affichage de chaque entrée du formulaire
-    //     //on obtient un tableau avec la premiere valeur est le name de l'input
-    //     //seconde valeur est la valeur dans l'input
-    //     console.log(entry);
-    // }
 
-    //creation d'un tableau avec les valerurs du formulaire
-    //conversion du tableau en objet
-    // const obj = Array.from(entries).reduce((acc, value ) => {
-    //     acc[value[0]] = value[1];
-    //     return acc;
-    // }, {});
-    //on obtient un objet js qui contient toutes les valeurs du formulaire
-    // console.log(obj);
-    
-    //methode opti
-    const obj = Object.fromEntries(entries);
-    // console.log(obj);
+    //on vient convertir les données dans un objet au format JSON
+    const objJson = JSON.stringify(Object.fromEntries(formData.entries()));
 
-    //on convertis en objet json avant l'envoie sur le serveur
-    const json = JSON.stringify(obj);
-    // console.log(json);
+    
+
 })
 
 
-
+//11:2
 
 
 
