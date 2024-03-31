f2e802e9b7327e09b51321b7fda39e086013b15c
Clement Glt <guillemot.clement@protonmail.com>
2024-03-31 12:03:40 +0200

    ✨ Récupération et affichage des articles du back-end

--- src/index.js
+++ src/index.js
@@ -2,4 +2,38 @@ import './assets/styles/styles.scss';
 import './index.scss';
 import './assets/js/topbar.js';
 
-console.log('accueil test');
+const articleContainerElement = document.querySelector('.articles-container');
+
+const createArticles = (articles) => {
+    const articlesDOM = articles.map(article => {
+        const articleDOM = document.createElement('div');
+        articleDOM.classList.add('article');
+        articleDOM.innerHTML = `
+            <img src="${ article.img }" alt="profil">
+            <h2>${ article.title }</h2>
+            <p class="article-author">${ article.author } - ${ article.category }</p>
+            <p class="article-content">${ article.content }</p>
+            <div class="article-action">
+                <button class="btn btn-danger" data-id=${ article._id }>Supprimer</button>
+            </div> 
+        `;
+        return articleDOM;
+    });
+    articleContainerElement.innerHTML = '';
+    articleContainerElement.append(...articlesDOM);
+};
+
+//on définit la fonction pour récupérer les données du back end
+const fetchArticle = async () => {
+    try{
+        const response = await fetch("https:restapi.fr/api/articles");
+        const articles = await response.json();
+        console.log(articles);
+        createArticles(articles);
+    } catch(e){
+        console.log('erreur : ', e);
+    }
+}
+
+//on appelle la fonction pour afficher les données
+fetchArticle();
